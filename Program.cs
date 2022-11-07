65e1eb219f8b733eb23f2fee7c0f9a6a74cb5a1a
01Joseph-Hwang10 <joseph95501@gmail.com>
2022-11-07 12:49:23 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using OnnxRuntime.ResNet.Template.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnnxRuntime.ResNet.Template
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Read paths
+            string modelFilePath = @"model\u2net.onnx";
+            string imageFilePath = @"data\test.jpg";
+
+            var input = ImageHelper.GetImageTensorFromPath(imageFilePath);
+            var result = ModelHelper.GetPredictions(input, modelFilePath);
+
+            // Print results to console
+            Console.WriteLine("--------------------------------------------------------------");
+        }
+    }
+}
---
+++ utils/ModelHelper.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace OnnxRuntime.ResNet.Template.utils
+{
+    public static class ModelHelper
+    {
+        public static Image<Rgb24> GetPredictions(Tensor<float> input, string modelFilePath)
+        {
+            // Setup inputs
+            var inputs = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("images", input)
+            };
+
+            // Run inference
+            var session = new InferenceSession(modelFilePath);
+            var results = session.Run(inputs).First();
+
+            int width = 224;
+            int height = 224;
+
+            var test = results.Value as DenseTensor<float>; 
+
+
+            var result = new Image<Rgb24>(width, height);
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    result[x, y] = new Rgb24(
+                        (byte)Math.Clamp(test[ y, x], 0, 255),
+                        (byte)Math.Clamp(test[y, x] , 0, 255),
+                        (byte)Math.Clamp(test[ y, x] , 0, 255)
+                    );
+                }
+            }
+            result.Save("test.jpg");
+
+            return result;
+        }
+
+
+    }
+}
